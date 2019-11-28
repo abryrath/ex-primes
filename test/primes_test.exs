58a04eadf46487dae72755ff84d5def640efc49c
Abry Rath <abryrath@gmail.com>
2019-11-28 07:10:28 -0500

    Correcting test case

--- test/primes_test.exs
+++ test/primes_test.exs
@@ -1,8 +1,13 @@
 defmodule PrimesTest do
   use ExUnit.Case
-  doctest Primes
+  # doctest Primes
 
-  test "greets the world" do
-    assert Primes.hello() == :world
+  test "first 25 primes by stream" do
+    actual = Primes.stream
+    |> Stream.take(25)
+    |> Enum.to_list
+
+    expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    assert actual == expected
   end
 end
